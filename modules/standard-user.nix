a30a4e8f074d165697e312cceff739db90ab7d6b
Kegan Riley Edwards <keganedwards@proton.me>
2025-08-13 20:59:58 -0400

    reapplying hooks

--- modules/standard-user.nix
+++ modules/standard-user.nix
@@ -2,16 +2,15 @@
 {
   username,
   fullName,
-flakeConstants,
+  flakeConstants,
   ...
 }: {
-
   users.users.${username} = {
     isNormalUser = true;
     description = fullName;
     extraGroups = ["keyd" "input" "uinput" "networkmanager" "wheel" "libvirtd" "video" "keys" "tss" "nix-admins"];
   };
   home-manager.users.${username} = {
-    home.stateVersion = flakeConstants.stateVersion; 
+    home.stateVersion = flakeConstants.stateVersion;
   };
 }
