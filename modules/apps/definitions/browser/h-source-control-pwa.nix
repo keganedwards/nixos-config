9728d62c994448c03751429b2766d2542bebe1ae
Kegan Riley Edwards <keganedwards@proton.me>
2025-08-29 14:09:48 -0400

    making github a webpage rather than a pwa

--- modules/apps/definitions/browser/h-source-control-pwa.nix
+++ modules/apps/definitions/browser/h-source-control-pwa.nix
@@ -1,7 +1,6 @@
-# modules/home-manager/apps/definitions/browser/h-source-control-pwa.nix
-{
-  type = "pwa";
-  id = "https://github.com";
-  appId = "brave-github.com__app-Default"; # Explicit, observed Sway app_id
+{constants, ...}: {
+  type = "flatpak";
+  id = constants.defaultWebbrowserFlatpakId;
   key = "h";
+  commandArgs = ''--new-window "https://github.com"'';
 }
