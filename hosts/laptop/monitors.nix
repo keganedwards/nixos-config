ed2052b966f6bc631e6e391fa212fb37a63c3ade
Kegan Riley Edwards <keganedwards@proton.me>
2025-10-02 10:25:09 -0400

    removing way-displays logging

--- hosts/laptop/monitors.nix
+++ hosts/laptop/monitors.nix
@@ -6,18 +6,11 @@
         {
           NAME_DESC = "!^eDP-1$";
 
-          # Create a list of explicit port names you use.
-          # This works because we proved an exact match succeeds.
           IF = [
             {PLUGGED = ["HDMI-A-2"];}
-            # Add other specific ports if you have them
-            # { PLUGGED = ["DP-1"]; },
-            # { PLUGGED = ["USB-C-1"]; },
           ];
         }
       ];
-
-      LOG_THRESHOLD = "INFO";
     };
   };
 }
