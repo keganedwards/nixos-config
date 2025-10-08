d06cd19fd505715f809f99c5032d00a8ab496a6a
Kegan Riley Edwards <keganedwards@proton.me>
2025-10-08 09:40:49 -0400

    removing redundant hm xdg.nix

--- modules/xdg/default.nix
+++ modules/xdg/default.nix
@@ -1,10 +1,5 @@
-# This file bridges the system and home-manager configurations for 'xdg'.
-{username, ...}: {
-  # System-level configurations for xdg
-  imports = [./system.nix];
-
-  # User-level (home-manager) configurations for xdg
-  home-manager.users.${username} = {
-    imports = [./hm.nix];
-  };
-}
+ {
+imports = [
+                 ./system.nix
+         ];
+ }
