0a1e7f5c0d2e62d4cc9324db4097161ab6d86e96
Kegan Riley Edwards <keganedwards@proton.me>
2025-10-16 20:34:09 -0400

    update tlp

--- hosts/laptop/tlp.nix
+++ hosts/laptop/tlp.nix
@@ -1,9 +1,11 @@
 {
-  services.tlp.enable = true; # turns on TLP’s systemd service
-  services.tlp.settings = {
-    CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-    CPU_SCALING_GOVERNOR_ON_AC = "performance";
-    START_CHARGE_THRESH_BAT0 = 40; # optional battery‑health caps
-    STOP_CHARGE_THRESH_BAT0 = 80;
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      START_CHARGE_THRESH_BAT0 = 50;
+      STOP_CHARGE_THRESH_BAT0 = 80;
+    };
   };
 }
