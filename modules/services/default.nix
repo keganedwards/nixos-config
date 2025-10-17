cf8dd5b3c48ffc7b5efc08f9fe931591a365a5e6
Kegan Riley Edwards <keganedwards@proton.me>
2025-10-17 13:18:04 -0400

    cleaned up code

--- modules/services/default.nix
+++ modules/services/default.nix
@@ -1,10 +1,6 @@
-# This file bridges the system and home-manager configurations for 'services'.
 {username, ...}: {
-  # System-level configurations for services
-  imports = [./system/default.nix];
-
-  # User-level (home-manager) configurations for services
+  imports = [./system];
   home-manager.users.${username} = {
-    imports = [./hm/default.nix];
+    imports = [./hm];
   };
 }
