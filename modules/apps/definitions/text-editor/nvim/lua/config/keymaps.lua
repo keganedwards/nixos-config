933b4ba8bad99cb1fbcdc5d9053bf33a502334a3
Kegan Riley Edwards <keganedwards@proton.me>
2025-10-19 21:33:52 -0400

    moving vim keybindings to leader

--- modules/apps/definitions/text-editor/nvim/lua/config/keymaps.lua
+++ modules/apps/definitions/text-editor/nvim/lua/config/keymaps.lua
@@ -1,10 +1,10 @@
 local keymap = vim.keymap.set
 
--- File navigation with FzfLua
-keymap("n", "<C-t>", "<cmd>FzfLua files<CR>", { desc = "Find files", silent = true })
-keymap("n", "<leader>fg", "<cmd>FzfLua live_grep<CR>", { desc = "Live grep" })
-keymap("n", "<leader>fb", "<cmd>FzfLua buffers<CR>", { desc = "Find buffers" })
-keymap("n", "<leader>fh", "<cmd>FzfLua help_tags<CR>", { desc = "Help tags" })
+-- File navigation with FzfLua using "\" as the leader key
+keymap("n", "\\t", "<cmd>FzfLua files<CR>", { desc = "Find files", silent = true })
+keymap("n", "\\fg", "<cmd>FzfLua live_grep<CR>", { desc = "Live grep" })
+keymap("n", "\\fb", "<cmd>FzfLua buffers<CR>", { desc = "Find buffers" })
+keymap("n", "\\fh", "<cmd>FzfLua help_tags<CR>", { desc = "Help tags" })
 
 -- Better window navigation
 keymap("n", "<C-h>", "<C-w>h", { desc = "Go to left window" })
@@ -13,17 +13,17 @@ keymap("n", "<C-k>", "<C-w>k", { desc = "Go to upper window" })
 keymap("n", "<C-l>", "<C-w>l", { desc = "Go to right window" })
 
 -- Move lines
-keymap("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
-keymap("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
+keymap("v", "\\k", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
+keymap("v", "\\j", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
 
 -- DAP
 if nixCats('debug') then
-  keymap("n", "<F5>", require("dap").continue, { desc = "Debug: Continue" })
-  keymap("n", "<F10>", require("dap").step_over, { desc = "Debug: Step Over" })
-  keymap("n", "<F11>", require("dap").step_into, { desc = "Debug: Step Into" })
-  keymap("n", "<F12>", require("dap").step_out, { desc = "Debug: Step Out" })
-  keymap("n", "<leader>b", require("dap").toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
-  keymap("n", "<leader>B", function()
+  keymap("n", "\\dc", require("dap").continue, { desc = "Debug: Continue" })
+  keymap("n", "\\do", require("dap").step_over, { desc = "Debug: Step Over" })
+  keymap("n", "\\di", require("dap").step_into, { desc = "Debug: Step Into" })
+  keymap("n", "\\du", require("dap").step_out, { desc = "Debug: Step Out" })
+  keymap("n", "\\b", require("dap").toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
+  keymap("n", "\\B", function()
     require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
   end, { desc = "Debug: Set Conditional Breakpoint" })
 end
